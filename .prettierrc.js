caebe82cd21564e417d4e95db78fe82c888fbc42
Naz <n@naz.io>
2022-12-15 15:05:28 +0400

    initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: false,
+  quoteProps: "as-needed",
+  jsxSingleQuote: false,
+  trailingComma: "es5",
+  jsxBracketSameLine: false,
+  arrowParens: "always",
+  plugins: [require("prettier-plugin-tailwindcss")],
+}
